17dc276558c4d408056b6a07ebd3eaf9e258c946
Yasmin <yasminor@gmail.com>
2016-08-25 10:35:19 -0700

    Completed implementation of stacks

--- Stack.rb
+++ Stack.rb
@@ -2,19 +2,24 @@ class Stack
   def initialize
     @store = Array.new
   end
-  
+
   def push(element)
+    @store.push(element) # @store << element
   end
- 
+
   def pop
+    return @store.pop # ruby has a method that pops off the last element in an array
   end
 
   def top
+    return @store.last
   end
- 
+
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0 # return @store.empty? # return @store.length == 0
   end
 end
